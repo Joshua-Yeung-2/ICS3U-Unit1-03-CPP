5bf9867b3046929a26d8cf0a8bcdbaf2f615d90d
Ubuntu <ubuntu@ip-172-31-7-96.ec2.internal>
2021-10-21 16:14:40 +0000

    some commit message

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Joshua Yeung All rights reserved
+//
+// Created by: Joshua Yeung
+// Created on: Sept 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+}
